74a672ee197e6a396846f4999de85c58755391e1
Jacob Kiesel <jacob@bitcrafters.co>
2025-05-30 23:56:11 -0600

    add async-nats to deps, refine CLI experience for server admins

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,11 +3,18 @@
 //! Checkout the `README.md` for guidance.
 
 use std::{
-    cell::RefCell, fs, io, net::SocketAddr, panic, path::PathBuf, sync::Arc, time::Duration,
+    cell::RefCell,
+    fs, io,
+    net::{IpAddr, Ipv4Addr, SocketAddr as StdSocketAddr},
+    panic,
+    path::PathBuf,
+    sync::Arc,
+    time::Duration,
 };
 
 use anyhow::{Context, Result, bail};
 use clap::Parser;
+use futures_util::{StreamExt, stream::FuturesUnordered};
 use hyper::{Request, body::Incoming};
 use hyper_util::{
     rt::{TokioExecutor, TokioIo},
@@ -19,10 +26,14 @@ use rustls::{
     ServerConfig,
     pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer},
 };
-use tokio::runtime;
+use tokio::{
+    net::TcpListener,
+    runtime,
+    sync::oneshot,
+};
 use tokio_rustls::TlsAcceptor;
 use tower::Service as _;
-use tracing::{error, info};
+use tracing::{error, info, level_filters::LevelFilter, warn};
 
 mod api;
 mod database;
@@ -31,20 +42,30 @@ mod database;
 #[clap(name = "server")]
 struct Opt {
     /// file to log TLS keys to for debugging
-    #[clap(long = "keylog")]
+    #[clap(long)]
     keylog: bool,
     /// TLS private key in PEM format
-    #[clap(short = 'k', long = "key", requires = "cert")]
+    #[clap(short = 'k', long, requires = "cert")]
     key: Option<PathBuf>,
     /// TLS certificate in PEM format
-    #[clap(short = 'c', long = "cert", requires = "key")]
+    #[clap(short = 'c', long, requires = "key")]
     cert: Option<PathBuf>,
-    /// Address to listen on
-    #[clap(long = "listen", default_value = "[::1]:4433")]
-    listen: SocketAddr,
-    /// Maximum number of concurrent connections to allow
-    #[clap(long = "connection-limit")]
-    connection_limit: Option<usize>,
+    /// Address(es) to listen on, can be either IPv4 or IPv6.
+    /// Pass multiple times to listen on more than one address.
+    #[clap(long, default_values_t = [IpAddr::V4(Ipv4Addr::UNSPECIFIED)])]
+    listen_addr: Vec<IpAddr>,
+
+    /// The network port to use for all given listen addresses.
+    #[clap(long, default_value_t = 443)]
+    port: u16,
+    /// By default Aspen mandates the use of HTTPS with TLS for all communications.
+    /// Aspen won't even issue a redirect over an insecure connection.
+    /// --no-https will invert this behavior, instead Aspen will never encrypt communications.
+    /// Exposing this mode to production users is wildly insecure and strongly discouraged. However,
+    /// if Aspen is behind a reverse proxy or other middleware that provides its own HTTPS you may
+    /// find this useful in a production environment.
+    #[clap(long)]
+    no_https: bool,
 }
 
 thread_local! {
@@ -55,7 +76,13 @@ fn main() {
     let _ = dotenvy::dotenv();
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::builder()
-            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_env_filter(
+                tracing_subscriber::EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .with_env_var("ASPEN_LOG")
+                    .from_env()
+                    .expect("invalid logging filter set in env var ASPEN_LOG"),
+            )
             .finish(),
     )
     .unwrap();
@@ -78,6 +105,15 @@ fn main() {
 }
 
 async fn run(options: Opt) -> Result<()> {
+    let (exit_tx, mut exit_rx) = oneshot::channel();
+    let mut exit_tx = Some(exit_tx);
+    ctrlc::set_handler(move || {
+        let Some(exit_tx) = exit_tx.take() else {
+            return;
+        };
+        let _ = exit_tx.send(());
+        info!("Shutdown signal received, shutting down...");
+    })?;
     let (certs, key) = if let (Some(key_path), Some(cert_path)) = (&options.key, &options.cert) {
         let key = fs::read(key_path).context("failed to read private key")?;
         let key = if key_path.extension().is_some_and(|x| x == "der") {
@@ -128,49 +164,94 @@ async fn run(options: Opt) -> Result<()> {
     };
 
     let app = api::make_router();
+    let tls_acceptor = (!options.no_https)
+        .then(|| -> Result<TlsAcceptor> {
+            // Setup TLS config
+            let mut server_config = ServerConfig::builder()
+                .with_no_client_auth()
+                .with_single_cert(certs, key)?;
+            server_config.alpn_protocols =
+                vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
+            if options.keylog {
+                server_config.key_log = Arc::new(rustls::KeyLogFile::new());
+            }
+            Ok(TlsAcceptor::from(Arc::new(server_config)))
+        })
+        .transpose()?;
 
-    let listener = tokio::net::TcpListener::bind(options.listen).await?;
-
-    // Setup TLS config
-    let mut server_config = ServerConfig::builder()
-        .with_no_client_auth()
-        .with_single_cert(certs, key)?;
-    server_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
-    if options.keylog {
-        server_config.key_log = Arc::new(rustls::KeyLogFile::new());
+    let mut listeners = Vec::new();
+    for addr in options.listen_addr {
+        match TcpListener::bind(StdSocketAddr::new(addr, options.port)).await {
+            Ok(listener) => {
+                info!("listening on {}", listener.local_addr()?);
+                listeners.push(listener);
+            }
+            Err(e) => warn!("unable to bind listen address {addr} due to {e}"),
+        }
     }
-    let tls_acceptor = TlsAcceptor::from(Arc::new(server_config));
 
-    info!("listening on {}", listener.local_addr()?);
+    if listeners.is_empty() {
+        bail!("all listen addresses failed to bind")
+    }
+
+    if options.no_https {
+        warn!("--no-https enabled, server is not encrypting anything in transit");
+    }
+
     loop {
-        let (socket, _remote_addr) = match listener.accept().await {
-            Ok(s) => s,
-            Err(e) => {
-                error!("TCP I/O error {e}");
-                continue;
+        let mut listeners = FuturesUnordered::from_iter(listeners.iter().map(|l| l.accept()));
+        let socket = tokio::select! {
+            maybe_socket = listeners.next() => {
+                match maybe_socket {
+                    Some(Ok((socket, _remote_addr))) => socket,
+                    Some(Err(e)) => {
+                        error!("TCP I/O error {e}");
+                        continue;
+                    }
+                    None => {
+                        unreachable!("listeners is not empty, and exactly one value is pulled from it.")
+                    }
+                }
+            },
+            _ = &mut exit_rx => {
+                break Ok(());
             }
         };
         let tls_acceptor = tls_acceptor.clone();
         let service = app.clone();
         tokio::spawn(async move {
-            let tls_stream = match tls_acceptor.accept(socket).await {
-                Ok(tls_stream) => tls_stream,
-                Err(e) => {
-                    error!("error establishing TLS {e}");
-                    return;
-                }
-            };
-            let socket = TokioIo::new(tls_stream);
-
             let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
                 service.clone().call(request)
             });
 
-            if let Err(e) = server::conn::auto::Builder::new(TokioExecutor::new())
-                .serve_connection_with_upgrades(socket, hyper_service)
-                .await
-            {
-                error!("failed to serve connection {e}");
+            /// Using a macro to do compile time duck typing over TlsStream and TcpStream.
+            macro_rules! handle_stream {
+                ($stream:expr) => {{
+                    let socket = TokioIo::new($stream);
+
+                    if let Err(e) = server::conn::auto::Builder::new(TokioExecutor::new())
+                        .serve_connection_with_upgrades(socket, hyper_service)
+                        .await
+                    {
+                        error!("failed to serve connection {e}");
+                    }
+                }};
+            }
+
+            match &tls_acceptor {
+                Some(tls_acceptor) => match tls_acceptor.accept(socket).await {
+                    Ok(tls_stream) => {
+                        handle_stream!(tls_stream)
+                    }
+                    Err(e) => {
+                        error!("error establishing TLS {e}");
+                        return;
+                    }
+                },
+                None => {
+                    // no_https enabled, send unencrypted.
+                    handle_stream!(socket)
+                }
             }
         });
     }
